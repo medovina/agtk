3ada2fbc68e5a93c6a5ba814c6bb76622e06c991
Adam Dingle <adam@medovina.org>
2025-02-12 16:10:33 +0100

    Add ColView class

---
+++ AGtk/ColView.cs
@@ -0,0 +1,95 @@
+using GObject;
+using Gtk;
+
+namespace AGtk;
+
+[Subclass<GObject.Object>]
+partial class Row {
+    public object[] values;
+
+    public Row(object[] values) : this() {
+        this.values = values;
+    }
+}
+
+// A ListItemFactory that creates a Label with text from a specific column.
+class LabelFactory : SignalListItemFactory {
+    int index;
+
+    public LabelFactory(int index) : base([]) {
+        this.index = index;
+        OnSetup += on_setup;
+        OnBind += on_bind;
+    }
+
+    private void on_setup(SignalListItemFactory sender, SetupSignalArgs args) {
+        ListItem listItem = (ListItem) args.Object;
+        Label label = Label.New(null);
+        label.Halign = Align.Start;
+        listItem.Child = label;
+    }
+
+    private void on_bind(SignalListItemFactory sender, BindSignalArgs args) {
+        ListItem listItem = (ListItem) args.Object;
+        Label label = (Label) listItem.Child!;
+        Row row = (Row) listItem.Item!;
+        label.SetText(row.values[index].ToString()!);
+    }
+}
+
+public class ColView : ColumnView {
+    string[] names;
+
+    Gio.ListStore store;
+    FilterListModel filter_model;
+    CustomFilter filter;
+
+    int? filter_column = null;
+    string filter_text = "";
+
+    public ColView(string[] names) {
+        this.names = names;
+        
+        store = Gio.ListStore.New(Row.GetGType());
+        for (int i = 0 ; i < names.Length ; ++i)
+            AppendColumn(ColumnViewColumn.New(names[i], new LabelFactory(i)));
+
+        filter = CustomFilter.New(is_row_visible); 
+        filter_model = FilterListModel.New(store, filter);
+        Model = SingleSelection.New(filter_model);
+    }
+
+    public string? FilterColumn {
+        get => filter_column == null ? null : names[(int) filter_column];
+
+        set {
+            if (value == null)
+                filter_column = null;
+            else {
+                filter_column = Array.IndexOf(names, value);
+                if (filter_column < 0)
+                    throw new Exception("column not found");
+            }
+            filter.Changed(FilterChange.Different);
+        }
+    }
+
+    public string FilterText {
+        get => filter_text;
+        set {
+            filter_text = value;
+            filter.Changed(FilterChange.Different);
+        }
+    }
+
+    public void Add(object[] values) {
+        store.Append(new Row(values));
+    }
+
+    bool is_row_visible(GObject.Object item) {
+        if (filter_column is int col) {
+            string s = ((Row) item).values[col].ToString()!;
+            return s.Contains(FilterText, StringComparison.OrdinalIgnoreCase);
+        } else return true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,52 @@
+# AGtk
+
+AGtk is a library of helper classes for GTK 4 in C#.  Well, actually at the moment there's only one helper class: AGtk.ColView.  But perhaps I'll add more in the future.
+
+## AGtk.ColView
+
+AGtk.ColView is a subclass of the Gtk.ColumnView class, which displays a multi-column list.  Gtk.ColumnView has a complicated API, so AGtk.ColView exists to make it much easier to use.  Here's a brief code sample to show how easy it is:
+
+```C#
+AGtk.ColView col_view = new(["year", "title", "director"]);
+
+col_view.Add([1981, "Raiders of the Lost Ark", "Steven Spielberg"]);
+col_view.Add([1994, "The Shawshank Redemption", "Frank Darabont"]);
+col_view.Add([1995, "Clueless", "Amy Heckerling"]);
+...
+
+my_window.Child = col_view;
+```
+
+That will produce a column view that looks like this:
+
+![column view](col_view.png)
+
+### Adding filtering
+
+AGtk.ColView supports dynamic filtering.  Typically you'll use this with a search box: as the user types in the box, rows will be filtered.  Here's how to do this in code:
+
+```C#
+// specify the column to use for filtering
+col_view.FilterColumn = "title";
+
+SearchEntry entry = new();
+entry.OnSearchChanged += on_search_changed;
+
+...
+
+void on_search_changed(SearchEntry sender, EventArgs args) {
+    col_view.FilterText = entry.GetText();
+}
+```
+
+### Sorting
+
+It would be nice if the user could click a column header to sort by that column.  Unfortunately that's not yet possible due to [limitations in the Gir.Core binding for GTK 4](https://github.com/gircore/gir.core/issues/1180).  I hope I'll be able to work with the Gir.Core developers to improve this situation before long.
+
+## Using the library
+
+Perhaps I'll publish a wget package at some point, but this is an early work in progress so I haven't done that yet.  In the meantime, you can clone this repository to your machine, then use the `dotnet add reference` command to add a reference to agtk.csproj in your project:
+
+```
+$ dotnet add reference /path/to/agtk.csproj
+```
