c88559076584f3c154b7222f09b893793abce81a
Adam Dingle <adam@medovina.org>
2025-02-18 20:51:15 +0100

    Add right click event

--- AGtk/ColView.cs
+++ AGtk/ColView.cs
@@ -5,10 +5,29 @@ namespace AGtk;
 
 [Subclass<GObject.Object>]
 public partial class Row {
-    public readonly object[] Values;
+    ColView? col_view;      // optional to suppress compiler warning
+    public readonly object[] Values = [];
+    Dictionary<int, Label> labels = [];
 
-    public Row(object[] values) : this() {
+    public Row(ColView col_view, object[] values) : this() {
+        this.col_view = col_view;
         this.Values = values;
+    }
+
+    public void add_label(int column, Label label) {
+        labels[column] = label;
+        label.SetText(Values[column].ToString()!);
+    }
+
+    public bool contains_label(Label label) => labels.ContainsValue(label);
+
+    public void SetValue(int col, object o) {
+        if (col < 0 || col >= Values.Length)
+            throw new Exception("index out of bounds");
+
+        col_view!.validate(col, o);
+        Values[col] = o;
+        labels[col].SetText(o.ToString()!);
     }
 }
 
@@ -33,16 +52,18 @@ class LabelFactory : SignalListItemFactory {
         ListItem listItem = (ListItem) args.Object;
         Label label = (Label) listItem.Child!;
         Row row = (Row) listItem.Item!;
-        label.SetText(row.Values[index].ToString()!);
+        row.add_label(index, label);
     }
 }
 
 public delegate void SelectionChanged(uint rowIndex);
 
-public class RowList(Gio.ListStore store) {
+public delegate void RightClick(int x, int y, uint rowIndex);
+
+public class RowList(Gio.ListModel model) {
     public Row this[uint i] {
         get {
-            object? o = store.GetObject(i);
+            object? o = model.GetObject(i);
             if (o == null)
                 throw new Exception("out of bounds");
             else
@@ -63,6 +84,7 @@ public class ColView : ColumnView {
     string filter_text = "";
 
     public event SelectionChanged? OnSelectionChanged;
+    public event RightClick? OnRightClick;
 
     public ColView(params string?[] names) {
         this.names = names;
@@ -87,6 +109,11 @@ public class ColView : ColumnView {
 
         Signal<SingleSelection> signal = new("selection-changed", "selection-changed");
         signal.Connect(selection_model, on_selection_changed);
+
+        GestureClick g = new();
+        g.Button = 3;
+        g.OnReleased += on_right_click;
+        AddController(g);
     }
 
     int compare(int column, nint a, nint b) {
@@ -97,9 +124,14 @@ public class ColView : ColumnView {
         return ((IComparable) objA).CompareTo(objB);
     }
 
-    public uint SelectedIndex => selection_model.Selected;
+    public uint SelectedIndex {
+        get => selection_model.Selected;
+        set { selection_model.Selected = value; }
+    }
 
-    public RowList Rows => new RowList(store);
+    public RowList Rows => new RowList(selection_model);
+
+    public uint RowCount => selection_model.GetNItems();
 
     public int? FilterColumn {
         get => filter_column;
@@ -118,19 +150,35 @@ public class ColView : ColumnView {
         }
     }
 
+    public void validate(int col, object val) {
+        if (names[col] != null && val is not IComparable)
+            throw new Exception("value in visible column must be comparable");
+    }
+
     public void Add(params object[] values) {
         if (values.Length != names.Length)
             throw new Exception("number of values doesn't match number of columns");
             
         for (int i = 0; i < values.Length; ++i)
-            if (names[i] != null && values[i] is not IComparable)
-                throw new Exception("value in visible column must be comparable");
+            validate(i, values[i]);
 
-        store.Append(new Row(values));
+        store.Append(new Row(this, values));
     }
 
     public void Clear() {
         store.RemoveAll();
+    }
+
+    uint store_index(Row row) {
+        for (uint i = 0 ; i < store.GetNItems(); ++i)
+            if (store.GetObject(i) == row)
+                return i;
+
+        throw new Exception("row not found");
+    }
+
+    public void DeleteRow(uint index) {
+        store.Remove(store_index(Rows[index]));
     }
 
     bool is_row_visible(GObject.Object item) {
@@ -143,5 +191,23 @@ public class ColView : ColumnView {
     void on_selection_changed(SingleSelection sender, EventArgs args) {
         if (OnSelectionChanged != null)
             OnSelectionChanged(SelectedIndex);
+    }
+
+    uint? label_row(Label label) {
+        for (uint i = 0; i < RowCount ; ++i)
+            if (Rows[i].contains_label(label))
+                return i;
+        return null;
+    }
+
+    void on_right_click(GestureClick sender, GestureClick.ReleasedSignalArgs args) {
+        if (OnRightClick != null) {
+            Widget? w = Pick(args.X, args.Y, 0);
+            Label? label = w as Label;
+            if (label == null && w != null)
+                label = w.GetFirstChild() as Label;
+            if (label != null && label_row(label) is uint index)
+                OnRightClick((int) args.X, (int) args.Y, index);
+        }
     }
 }
